c0b11cb828c7737a9e75ff7ae98e97c920be0ab4
Nick Stevens <nick@bitcurry.com>
2018-09-03 14:07:12 -0500

    Add initial nastevens user and admin group
    Signed-off-by: Nick Stevens <nick@bitcurry.com>

---
+++ iam.tf
@@ -0,0 +1,39 @@
+# Policies
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length = 16
+  require_lowercase_characters = true
+  require_uppercase_characters = true
+  require_numbers = true
+  require_symbols = true
+  allow_users_to_change_password = true
+  hard_expiry = false
+  max_password_age = 90
+}
+
+# Users
+resource "aws_iam_user" "nastevens" {
+  name = "nastevens"
+}
+
+# Groups and group membership
+resource "aws_iam_group" "administrators" {
+  name = "Administrators"
+}
+
+resource "aws_iam_group_policy_attachment" "administrators_admin" {
+  group = "${aws_iam_group.administrators.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "administrators_change_pw" {
+  group = "${aws_iam_group.administrators.name}"
+  policy_arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
+}
+
+resource "aws_iam_group_membership" "administrators_membership" {
+  name = "AdministratorsMembership"
+  users = [
+    "${aws_iam_user.nastevens.name}"
+  ]
+  group = "${aws_iam_group.administrators.name}"
+}
